a8f3802e5c7df2fd3c0fd1812dadea500f7e2005
Dhruv Amin <amin.dhruv@gmail.com>
2024-10-19 00:09:04 -0400

    feat: complete learning

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,38 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/edit-form";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Edit Invoice",
+};
+
+export default async function Page({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const [customers, invoice] = await Promise.all([
+    fetchCustomers(),
+    fetchInvoiceById(id),
+  ]);
+
+  if (!invoice) notFound();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
